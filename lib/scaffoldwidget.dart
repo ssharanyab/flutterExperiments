6c8629d35c36ec4fdd30be6b91fd7227e58e26e1
Sharanya <ssharanyab@gmail.com>
2021-06-07 13:19:43 +0530

    first commit

---
+++ lib/scaffoldwidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class scafwid extends StatelessWidget {
+  const scafwid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      drawer: Drawer(),
+    );
+  }
+}
+
